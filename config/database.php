a55f1e30c08988af6504406a7707ef34cfab73b9
Muhammad Reza Pahlevi Y <rezarubik17@gmail.com>
2019-08-06 14:22:47 +0700

    update database.php
    add oracle.php

--- config/database.php
+++ config/database.php
@@ -91,6 +91,19 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'oracle' => [
+            'driver'        => 'oracle',
+            'tns'           => env('DB_TNS', ''),
+            'host'          => env('DB_HOST', ''),
+            'port'          => env('DB_PORT', '1521'),
+            'database'      => env('DB_DATABASE', ''),
+            'username'      => env('DB_USERNAME', ''),
+            'password'      => env('DB_PASSWORD', ''),
+            'charset'       => env('DB_CHARSET', 'AL32UTF8'),
+            'prefix'        => env('DB_PREFIX', ''),
+            'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
+        ],
+
     ],
 
     /*
@@ -123,7 +136,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
